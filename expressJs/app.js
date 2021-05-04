a60692b41aa09355e21028130d7cfecd3d659626
Prateek Jain <pratkjn@gmail.com>
2021-05-04 11:17:43 +0530

    Express Routing

--- expressJs/app.js
+++ expressJs/app.js
@@ -3,23 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-//Below is the parsing the request body section
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//Below are the routing sections
-
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    console.log(req.body.title);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Welcome! Hello from Express</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
